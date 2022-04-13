27d9b2a6f7128ded6c3f6bb60a67592c22f3ee0f
Ton Machielsen--replace-all <ton@machielsen.net>
2022-04-13 17:39:29 +0200

    Whatever was updated

--- main.tf
+++ main.tf
@@ -57,20 +57,20 @@ resource "aws_instance" "mesos-master" {
       "CODENAME=$(lsb_release -cs)",
       "echo \"deb http://repos.mesosphere.com/$${DISTRO} $${CODENAME} main\" | sudo tee /etc/apt/sources.list.d/mesosphere.list",
       "sudo apt-get update",
-      "sudo apt-get -y install mesos marathon",
+      "sudo apt install mesos marathon -y",
       "echo \"1\" | sudo tee /etc/zookeeper/conf/myid",
       "echo \"zk://${aws_instance.mesos-master.public_ip}:2181/mesos\" | sudo tee /etc/mesos/zk",
       "echo \"server.1=${aws_instance.mesos-master.public_ip}:2888:3888\" | sudo tee /etc/zookeeper/conf/zoo.cfg",
       "echo ${aws_instance.mesos-master.public_ip} | sudo tee /etc/mesos-master/ip",
       "cp /etc/mesos-master/ip /etc/mesos-master/hostname",
       "echo MyCluster| sudo tee /etc/mesos-master/cluster",
-      "echo \"MARATHON_MASTER=zk://${aws_instance.mesos-master.public_ip}:2181/mesos\" | sudo tee /etc/default/marathon",
-      "echo \"MARATHON_ZK=zk://${aws_instance.mesos-master.public_ip}:2181/marathon\" | sudo tee /etc/default/marathon",
-      "service mesos-slave stop",
+      "echo \"MARATHON_MASTER=zk://${aws_instance.mesos-master.public_ip}:2181/mesos\" | sudo tee -a /etc/default/marathon",
+      "echo \"MARATHON_ZK=zk://${aws_instance.mesos-master.public_ip}:2181/marathon\" | sudo tee -a /etc/default/marathon",
+      "sudo service mesos-slave stop",
       "echo manual | tee /etc/init/mesos-slave.override",
-      "service zookeeper restart",
-      "service mesos-master restart",
-      "service marathon restart"
+      "sudo service zookeeper restart",
+      "sudo service mesos-master restart",
+      "sudo service marathon restart"
     ]
   }
 }
@@ -105,16 +105,16 @@ resource "aws_instance" "mesos-slave" {
       "DISTRO=$(lsb_release -is | tr '[:upper:]' '[:lower:]')",
       "CODENAME=$(lsb_release -cs)",
       "echo \"deb http://repos.mesosphere.com/$${DISTRO} $${CODENAME} main\" | sudo tee /etc/apt/sources.list.d/mesosphere.list",
-      "sudo apt-get update",
-      "apt-get -y install mesos",
-      "service zookeeper stop",
-      "echo manual | tee /etc/init/zookeeper.override",
-      "service mesos-master stop",
-      "echo manual | tee /etc/init/mesos-master.override",
+      "sudo apt update",
+      "sudo apt -y install mesos",
+      "sudo service zookeeper stop",
+      "echo manual | sudo tee /etc/init/zookeeper.override",
+      "sudo service mesos-master stop",
+      "echo manual | sudo tee /etc/init/mesos-master.override",
       "echo ${aws_instance.mesos-slave.public_ip} | sudo tee /etc/mesos-slave/ip",
-      "cp /etc/mesos-slave/ip /etc/mesos-slave/hostname",
+      "sudo cp /etc/mesos-slave/ip /etc/mesos-slave/hostname",
       "echo \"zk://${aws_instance.mesos-master.public_ip}:2181/mesos\" | sudo tee /etc/mesos/zk",
-      "service mesos-slave restart"
+      "sudo service mesos-slave restart"
     ]
   }
 }
--- outputs.tf
+++ outputs.tf
@@ -1,6 +1,12 @@
 output "master-public_ip" {
   value = aws_instance.mesos-master.public_ip
 }
+output "master-private_ip" {
+  value = aws_instance.mesos-master.private_ip
+}
 output "slave-public_ip" {
   value = aws_instance.mesos-slave.public_ip
+}
+output "slave-private_ip" {
+  value = aws_instance.mesos-slave.private_ip
 }
